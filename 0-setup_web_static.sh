d12137ae81b700feeb8f5c796a6e15037c86106d
AmineNeifer <amineneifer2000@gmail.com>
2020-04-08 21:49:47 +0100

    web servers preparing

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#commnt
+if [ ! -e "/usr/sbin/nginx" ]
+then
+        sudo apt-get -y update
+        sudo apt-get -y upgrade
+        sudo apt-get -y install nginx
+        sudo service nginx start
+fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "lela ya lela" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+if [ -e "/data/web_static/current" ]
+then
+    rm /data/web_static/current
+fi
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+new_str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"
+sudo sed -i " 25i $new_str" /etc/nginx/sites-enabled/default
+sudo service nginx restart
