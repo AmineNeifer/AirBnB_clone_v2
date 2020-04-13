c047e9052fecf71ebf940bc39c37c8dd5b6b176b
AmineNeifer <amineneifer2000@gmail.com>
2020-04-13 17:33:01 +0100

    bjeh raby

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,26 +17,33 @@ def do_deploy(archive_path):
     """
     distributes archive to web servers
     """
-    name = archive_path.split('/')[1]
-    nne = name.split(".")[0]
-    rel = "/data/web_static/releases"
-    cur = "/data/web_static/current"
-    if not put(archive_path, "/tmp/{}".format(name)).succeeded:
+    fname = archive_path.split('/')[-1]
+    r = put(archive_path, "/tmp/")
+    if r.failed:
         return False
-    if not run("mkdir -p {}/{}/".format(rel, nne)).succeeded:
+    path = '/data/web_static/releases/' + fname[:-4]
+    r = run('mkdir -p %s' % path)
+    if r.failed:
         return False
-    if not run("tar -xzf /tmp/{} -C {}/{}/".format(rel, name, nne)).succeeded:
+    r = run('tar -xzf /tmp/%s -C /data/web_static/releases/%s/' %
+            (fname, fname[:-4]))
+    if r.failed:
         return False
-    if not run("rm /tmp/{}".format(name)).succeeded:
+    r = run('rm /tmp/%s' % fname)
+    if r.failed:
         return False
-    what_to_mv = "{}/{}/web_static/*".format(rel, nne)
-    where_to_mv = "{}/{}/".format(rel, nne)
-    if not run("mv {} {}".format(what_to_mv, where_to_mv)).succeeded:
+    r = run('mv /data/web_static/releases/%s/web_static/*\
+    /data/web_static/releases/%s' % (fname[:-4], fname[:-4]))
+    if r.failed:
         return False
-    if not run("rm -rf {}/{}/web_static".format(rel, nne)).succeeded:
+    r = run('rm -rf /data/web_static/releases/%s/web_static' % fname[:-4])
+    if r.failed:
         return False
-    if not run("rm -rf /data/web_static/current").succeeded:
+    r = run('rm -rf /data/web_static/current')
+    if r.failed:
         return False
-    if not run("ln -s {}/{}/ {}".format(rel, nne, cur)).succeeded:
+    r = run('ln -s /data/web_static/releases/%s /data/web_static/current'
+            % fname[:-4])
+    if r.failed:
         return False
     return True
