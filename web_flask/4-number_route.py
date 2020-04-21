2fdf29b7999c00a5c134b76e1d828158951695bf
AmineNeifer <amineneifer2000@gmail.com>
2020-04-21 16:19:49 +0100

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""stating a simple flask web app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C %s' % text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+def only_python():
+    return 'Python is cool'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return 'Python %s' % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_(n):
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
